99deb57bf724dd113e77268139e041d263768205
mac <mac@macdeiMac.local>
2016-11-04 18:19:30 +0800

    add enum

---
+++ Enumerations.playground/Contents.swift
@@ -0,0 +1,77 @@
+//: Playground - noun: a place where people can play
+
+//枚举
+//枚举为一组相关的值定义了一个共同的类型
+enum CompassPoint {
+    case north
+    case south
+    case east
+    case west
+}
+
+enum Planet {
+    case mercury, venus, earth, mars, jupiter, saturn, uranus, neptune
+}
+
+var directionToHead = CompassPoint.west
+directionToHead = .east
+
+directionToHead = .south
+
+//使用Switch语句匹配枚举值
+switch directionToHead {
+case .north:
+    print("Lots of planets have a north")
+case .south:
+    print("Watch out for penguins")
+case .east:
+    print("Where the sun rises")
+case .west:
+    print("Where the skies are blue")
+}
+
+let somePlanet = Planet.earth
+switch somePlanet {
+case .earth:
+    print("Mostly harmless")
+default:
+    print("Not a safe place for humans")
+}
+
+//关联值
+enum Barcode {
+    case upc(Int, Int, Int, Int)
+    case qrCode(String)
+}
+var productBarcode = Barcode.upc(8, 88298, 93293, 9)
+productBarcode = .qrCode("ABCSIDOSADH")
+switch productBarcode {
+case .upc(let numberSystem, let manufacturer, let product, let check):
+    print("UPC: \(numberSystem), \(manufacturer), \(product), \(check).")
+case .qrCode(let productCode):
+    print("QR code: \(productCode)")
+}
+
+//如果一个枚举成员的所有关联值都被提取为常量，或者都被提取为变量，为了简洁，你可以只在成员名称前标注一个let或者var：
+switch productBarcode {
+case let .upc(numberSystem, manufacturer, product, check):
+    print("UPC: \(numberSystem), \(manufacturer), \(product), \(check)")
+case let .qrCode(productCode):
+    print("QR code :\(productCode)")
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
