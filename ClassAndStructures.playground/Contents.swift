30f3df036bc5d9fd81d046f6edb57526a3c10125
mac <mac@macdeiMac.local>
2016-11-08 18:01:33 +0800

    add property

--- ClassAndStructures.playground/Contents.swift
+++ ClassAndStructures.playground/Contents.swift
@@ -1,17 +1,98 @@
 //: Playground - noun: a place where people can play
 
+//类和结构体的对比
+/*
+ Swift 中类和结构体有很多共同点。共同处在于：
+ 
+ 定义属性用于存储值
+ 定义方法用于提供功能
+ 定义下标操作使得可以通过下标语法来访问实例所包含的值
+ 定义构造器用于生成初始化值
+ 通过扩展以增加默认实现的功能
+ 实现协议以提供某种标准功能。
+ 
+ 与结构体相比，类还有如下的附加功能：
+ 
+ 继承允许一个类继承另一个类的特征
+ 类型转换允许在运行时检查和解释一个类实例的类型
+ 析构器允许一个类实例释放任何其所被分配的资源
+ 引用计数允许对一个类的多次引用
+ */
 
 
+struct Resolution {
+    var width = 0
+    var height = 0
+}
+class VideoMode {
+    var resolution = Resolution()
+    var interlaced = false
+    var frameRate = 0.0
+    var name: String?
+}
+
+let someResolution = Resolution()
+let someVideoMode = VideoMode()
+print("The width if someResolution is \(someResolution.width)")
+print("The width of someVideoMode is \(someVideoMode.resolution.width)")
+someVideoMode.resolution.width = 1280
+print("The width of someVideoMode is now \(someVideoMode.resolution.width)")
+//与 Objective-C 语言不同的是，Swift 允许直接设置结构体属性的子属性
+
+//所有结构体都有一个自动生成的成员逐一构造器，用于初始化新结构体实例中成员的属性。与结构体不同，类实例没有默认的成员逐一构造器
+let vga = Resolution(width: 640, height: 480)
+
+//结构体和枚举是值类型
+//在 Swift 中，所有的基本类型：整数（Integer）、浮点数（floating-point）、布尔值（Boolean）、字符串（string)、数组（array）和字典（dictionary），都是值类型，并且在底层都是以结构体的形式所实现。
+let hd = Resolution(width: 1290, height: 1080)
+var cinema = hd
+cinema.width = 2048
+print("cinema is now \(cinema.width) pixels wide")
+print("hd is still \(hd.width) pixels wide")
+
+enum CompassPoint {
+    case North, South, East, West
+}
+var currentDirection = CompassPoint.West
+let rememberedDirection = currentDirection
+currentDirection = .East
+if rememberedDirection == .West {
+    print("The remembered direction is still .West")
+}
+
+//类是引用类型
+let tenEighty = VideoMode()
+tenEighty.resolution = hd
+tenEighty.interlaced = true
+tenEighty.name = "1080i"
+tenEighty.frameRate = 25.0
+
+let alsoTenEighty = tenEighty
+alsoTenEighty.frameRate = 30.0
+print("The frameRate property of tenEighty is now \(tenEighty.frameRate)")
+//需要注意的是tenEighty和alsoTenEighty被声明为常量而不是变量。然而你依然可以改变tenEighty.frameRate和alsoTenEighty.frameRate，因为tenEighty和alsoTenEighty这两个常量的值并未改变。它们并不“存储”这个VideoMode实例，而仅仅是对VideoMode实例的引用。所以，改变的是被引用的VideoMode的frameRate属性，而不是引用VideoMode的常量的值。
+
+//恒等运算符
+/*
+ 等价于（===）
+ 不等价于（!==）
+ */
+
+if tenEighty === alsoTenEighty {
+    print("tenEighty and alsoTenEighty refer to the same Resolution instance")
+}
+/*
+ 请注意，“等价于”（用三个等号表示，===）与“等于”（用两个等号表示，==）的不同：
+ 
+ “等价于”表示两个类类型（class type）的常量或者变量引用同一个类实例。
+ “等于”表示两个实例的值“相等”或“相同”，判定时要遵照设计者定义的评判标准，因此相对于“相等”来说，这是一种更加合适的叫法
+ */
+
+//指针
+//如果你有 C，C++ 或者 Objective-C 语言的经验，那么你也许会知道这些语言使用指针来引用内存中的地址。一个引用某个引用类型实例的 Swift 常量或者变量，与 C 语言中的指针类似，但是并不直接指向某个内存地址，也不要求你使用星号（*）来表明你在创建一个引用。Swift 中的这些引用与其它的常量或变量的定义方式相同。
 
 
+/*Swift 中，许多基本类型，诸如String，Array和Dictionary类型均以结构体的形式实现。这意味着被赋值给新的常量或变量，或者被传入函数或方法中时，它们的值会被拷贝。
 
-
-
-
-
-
-
-
-
-
-
+ Objective-C 中NSString，NSArray和NSDictionary类型均以类的形式实现，而并非结构体。它们在被赋值或者被传入函数或方法时，不会发生值拷贝，而是传递现有实例的引用。
+*/
