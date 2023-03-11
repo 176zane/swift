512808f48d70b14aab7490d8818b66b5544cd83d
176zane <176zane@gmail.com>
2023-03-11 21:54:52 +0800

    添加SwiftUI

---
+++ SwiftUI/Calculator/Calculator/CalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalculatorApp.swift
+//  Calculator
+//
+//  Created by Deep Thought on 2023/3/6.
+//
+
+import SwiftUI
+
+@main
+struct CalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
